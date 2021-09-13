11d427c11edff9d4e13a27a6c0ec99bea0c4f305
Alex <rmo84791@gmail.com>
2021-09-13 22:58:29 +0300

    добавил варианты поиска
    добавил варианты поиска через name

--- autologin.py
+++ autologin.py
@@ -6,11 +6,16 @@ try:
     browser = webdriver.Chrome()
     browser.get(link)
     time.sleep(3) # ждем пока прогрузится страница
-    button = browser.find_element_by_xpath("/html/body/header/div[1]/div/div/div[2]/div/div/a[2]").click() #жмем кнопку логин
+    btn_login = browser.find_element_by_xpath("/html/body/header/div[1]/div/div/div[2]/div/div/a[2]").click() #жмем кнопку логин
 
-    login_box = browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/form/div[1]/div/input').send_keys("user@phptravels.com")#вводим логин
-    pass_box = browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/form/div[2]/div[1]/input').send_keys("demouser")# вводим пароль
-    button_login = browser.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/div/form/div[3]/button").click()#логинимся
+    #login_box = browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/form/div[1]/div/input').send_keys("user@phptravels.com")#вводим логин. поиск через xpath
+    login_box = browser.find_element_by_name('email').send_keys("user@phptravels.com")#вводим логин. поиск через name 
+
+    #pass_box = browser.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/form/div[2]/div[1]/input').send_keys("demouser")# вводим пароль. поиск через xpath
+    pass_box = browser.find_element_by_name('password').send_keys("demouser") # вводим пароль. поиск через name 
+    
+    button_login = browser.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[2]/div/form/div[3]/button").click()#логинимся. поиск через xpath
+
 
 finally:
     # ожидание чтобы визуально оценить результаты прохождения скрипта
